dcc3856cbc1b67e6d405fc128be8d0bd9e042602
Arslan ALAN <Arslan ALAN>
2017-04-25 16:43:29 +0300

    Add picture element

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,26 @@ module.exports = function(grunt) {
       dev: {
         options: {
           sizes: [{
+              width: '320',
+              suffix: '_small',
+              quality: 30
+              },
+              {
+              width: '640',
+              suffix: '_medium',
+              quality: 30
+              },
+              {
+              width: '800',
+              suffix: '_large_1x',
+              quality: 30
+              },
+			        {
               width: '1600',
               suffix: '_large_2x',
               quality: 30
-              }]
+              }
+              ]
         },
 
         /*
